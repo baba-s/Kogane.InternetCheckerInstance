44fbda02be8ae32367e61bf5014f6d2e00591811
baba-s <shotababa1206@gmail.com>
2023-01-01 18:31:55 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Kogane.InternetCheckerInstance
+﻿# Kogane Internet Checker Instance
+
+インターネットに接続しているか確認できるクラス
+
+## 使用例
+
+```cs
+using Cysharp.Threading.Tasks;
+using Kogane;
+using UnityEngine;
+
+public sealed class Example : MonoBehaviour
+{
+    private readonly InternetCheckerInstance m_instance = new
+    (
+        "8.8.8.8", // Google Public DNS
+        "8.8.4.4", // Google Public DNS
+        "4.2.2.2", // Level 3 Communications
+        "4.2.2.3", // Level 3 Communications
+        "4.2.2.4"  // Level 3 Communications
+    );
+
+    private async UniTaskVoid Start()
+    {
+        Debug.Log( await m_instance.IsOnlineAsync( 5 ) );
+    }
+}
+```
---
+++ Runtime/InternetCheckerInstance.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+
+namespace Kogane
+{
+    public sealed class InternetCheckerInstance
+    {
+        private readonly IReadOnlyList<string> m_addresses;
+
+        public InternetCheckerInstance( params string[] addresses )
+            : this( ( IReadOnlyList<string> )addresses )
+        {
+        }
+
+        public InternetCheckerInstance( IReadOnlyList<string> addresses )
+        {
+            m_addresses = addresses;
+        }
+
+        public async UniTask<bool> IsOnlineAsync( float timeoutSeconds )
+        {
+            foreach ( var address in m_addresses )
+            {
+                var result = await PingUtils.SendAsync( address, timeoutSeconds );
+
+                if ( result.IsSuccess ) return true;
+            }
+
+            return false;
+        }
+    }
+}
